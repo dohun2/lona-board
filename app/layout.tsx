37eca105972bb2216536da564dec33f1707d0022
dohun2 <dohun_2@naver.com>
2025-05-04 23:53:27 +0900

    fix: Analytics 재설치

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-// import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/react";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import TopBoard from "@/components/TopBoard";
@@ -36,7 +36,7 @@ export default function RootLayout({
       >
         <TopBoard />
         {children}
-        {/* <Analytics /> */}
+        <Analytics />
         <BottomNavBar />
       </body>
     </html>
