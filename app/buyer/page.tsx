ab4551f6c4e779fe990bd3c7f598108559af5aa6
dohun2 <dohun_2@naver.com>
2025-05-08 18:22:32 +0900

    chore: 구매자 정보 상수값 변경

--- app/buyer/page.tsx
+++ app/buyer/page.tsx
@@ -1,6 +1,6 @@
 import BuyerTable from "@/components/BuyerTable";
 
-const buyerData = [{ content: "영방5강 아잠 40 그외 50", nickname: "이뚜둘" }];
+const buyerData = [{ content: "영방5강 40", nickname: "이뚜둘" }];
 
 const Buyer = () => {
   return <BuyerTable data={buyerData} />;
