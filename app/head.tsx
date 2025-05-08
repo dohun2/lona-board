4f609f3afc0f4dd0dfea9d2a9be1c1d5381bafcd
dohun2 <dohun_2@naver.com>
2025-05-08 17:16:50 +0900

    chore: Google Search Console 인증

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name='google-site-verification'
+        content='VUhALrgfBxRxPoIE5nbzM9CvOVnRPn4rMGUHWDOk38w'
+      />
+    </>
+  );
+}
