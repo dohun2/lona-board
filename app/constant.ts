5668e0d957ae11304504297a52b5c8b65c601b5e
dohun2 <dohun_2@naver.com>
2025-05-09 02:47:41 +0900

    chore: 상수 분리

---
+++ app/constant.ts
@@ -0,0 +1,64 @@
+export const HUNTING_GROUNDS = [
+  { name: "붉은 억새 평원", note: "전체", type: "field", control: false },
+  { name: "바르바스", note: "전체", type: "field", control: false },
+  {
+    name: "티리오사의 무덤 지하 1층",
+    note: "전체",
+    type: "dungeon",
+    control: false,
+  },
+  {
+    name: "티리오사의 무덤 지하 2층",
+    note: "전체",
+    type: "dungeon",
+    control: false,
+  },
+  {
+    name: "티리오사의 무덤 지하 3층",
+    note: "전체",
+    type: "dungeon",
+    control: true,
+  },
+  {
+    name: "죽은 자의 대지 1구역",
+    note: "전체",
+    type: "dungeon",
+    control: false,
+  },
+  {
+    name: "죽은 자의 대지 2구역",
+    note: "전체",
+    type: "dungeon",
+    control: true,
+  },
+  {
+    name: "죽은 자의 대지 3구역",
+    note: "전체",
+    type: "dungeon",
+    control: true,
+  },
+  {
+    name: "드라카스 화산",
+    note: "전체",
+    type: "dungeon",
+    control: false,
+  },
+  {
+    name: "월드 던전",
+    note: "전체",
+    type: "dungeon",
+    control: false,
+  },
+  {
+    name: "월드",
+    note: "클릭해서 범위,시간 확인",
+    type: "dungeon",
+    control: true,
+    url: "https://page.onstove.com/l9/global/view/10610805?boardKey=128093",
+  },
+];
+
+export const BUYER_LIST = [
+  { content: "영방5강 40", nickname: "이뚜둘" },
+  { content: "영방5강 아잠 40 나머지 50", nickname: "조태오" },
+];
